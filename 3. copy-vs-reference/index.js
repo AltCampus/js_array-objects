1a1fb1f09925852b315f836548ff5fd2acf680e8
Gangaprasad Mohite <shanky31@localhost.localdomain>
2019-09-15 21:03:36 +0530

    solved copy vs reference

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -5,6 +5,11 @@ var y = "abc";
 var a = x;
 var b = y;
 
+x= 10
+y= "abc"
+a= 10
+b= "abc"
+
 // Value of x, y, a, b
 var x = 10;
 var y = "abc";
@@ -13,19 +18,32 @@ var b = y;
 a = 5;
 b = "def";
 
+x = 10
+y = "abc"
+a = 5
+b = "def"
+
 // Value of arr & arrCopy
 var arr = [1];
 var arrCopy = arr;
 arr.push(2);
 
+arr = [1,2]
+arrCopy = [1, 2]
+
+
 // Value of obj
 var obj = { name: "ryan" };
 obj = { surname: "florance" };
+
+obj = {surname: "florance"}
 
 // What's the output.
 var arr = ["Hi"];
 var arr2 = arr;
 console.log(arr === arr2);
+
+true
 
 // What's the output.
 var arr1 = ["Hi!"];
@@ -33,9 +51,11 @@ var arr2 = ["Hi!"];
 console.log(arr1 === arr2);
 console.log(arr == arr2);
 
+false
+
 //What's the output
 console.log([10] === [10]);
-
+false
 // What's the output?
 
 function personDetails(person) {
@@ -55,12 +75,18 @@ var personObj2 = personDetails(personObj1);
 console.log(personObj1); // -> ?
 console.log(personObj2); // -> ?
 
+{name: "Alex", age: 25}
+{name: "John", age: 50}
+
+
 // Guess the output
 var oldArray = [];
 var object = {};
 object.newArray = oldArray;
 oldArray.push(10);
 console.log(object.newArray === oldArray);
+
+true
 
 // Guess the output
 var a = 5;
@@ -69,12 +95,18 @@ a = 10;
 console.log(a);
 console.log(b);
 
+10 
+5
+
 // What's the output?
 var a = {};
 var b = a;
 a.a = 1;
 console.log(a);
 console.log(b);
+
+{a: 1}
+{a: 1}
 
 // What's the output.
 var a = [];
@@ -83,6 +115,10 @@ a.push(1);
 console.log(a); // [1]
 console.log(b); // [1]
 console.log(a === b);
+
+[1]
+[1]
+true
 
 // Clone the object person in clone
 var person = {
@@ -94,4 +130,4 @@ var person = {
   }
 };
 
-var clone = {};
+var clone = {person};
